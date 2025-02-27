b596a318316646b3f85b92083beaa901d6a87b03
Arup <arup03.paul@gmail.com>
2025-02-27 14:35:06 +0530

    Add Readme

--- login_app/login_app/settings.py
+++ login_app/login_app/settings.py
@@ -122,7 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SESSION_ENGINE = "django.contrib.sessions.backends.db"  # Default session storage
-SESSION_COOKIE_AGE = 0  # Forces session to expire immediately on logout
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Session expires when browser closes
