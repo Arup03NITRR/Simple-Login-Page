dac1d67d010cead10506db091b5776ba41dbebe8
Arup <arup03.paul@gmail.com>
2025-02-27 13:54:50 +0530

    Adding signup view

--- login_app/accounts/views.py
+++ login_app/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 # Create your views here.
+def signup_view(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        if(User.objects.filter(username=username).exists()):
+            messages.error(request, "Username is already taken!")
+            return redirect('signup')
+        user=User.objects.create_user(username=username, password=password)
+        user.save()
+        messages.success(request, "Successfully signed up, Please login...")
+        return redirect('login')
+    return render(request, 'signup.html')
